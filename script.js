7383e57d64aa848bda6d7f003e5c7ae4a9f9a9f4
Talha Siddiqui <31357614+siddiquit97@users.noreply.github.com>
2021-11-27 12:52:45 -0600

    Update script.js
    Included catch statement

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ async function selectMediaStream() {
       videoElement.play();
     };
   } catch (error) {
-    // Catch Error Here
+    console.log("Error with getting media stream: " + error);
   }
 }
 
